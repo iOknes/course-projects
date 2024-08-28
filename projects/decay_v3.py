a68faf45a9e45158ce7ed350a199bb767f5b12ed
Iver Oknes <iver@oknes.no>
2024-08-28 11:48:51 +0200

    Fix imports to not use star imports

--- projects/decay_v3.py
+++ projects/decay_v3.py
@@ -1,12 +1,12 @@
-from numpy import *
+import numpy as np
 
 def solver(I, a, T, dt, theta):
     """Solve u'=-a*u, u(0)=I, for t in (0,T] with steps of dt."""
     dt = float(dt)            # avoid integer division
     Nt = int(round(T/dt))     # no of time intervals
     T = Nt*dt                 # adjust T to fit time step dt
-    u = zeros(Nt+1)           # array of u[n] values
-    t = linspace(0, T, Nt+1)  # time mesh
+    u = np.zeros(Nt+1)           # array of u[n] values
+    t = np.linspace(0, T, Nt+1)  # time mesh
 
     u[0] = I                  # assign initial condition
     for n in range(0, Nt):    # n=0,1,...,Nt-1
@@ -14,29 +14,29 @@ def solver(I, a, T, dt, theta):
     return u, t
 
 def u_exact(t, I, a):
-    return I*exp(-a*t)
+    return I*np.exp(-a*t)
 
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 
 def plot_numerical_and_exact(theta, I, a, T, dt):
     """Compare the numerical and exact solution in a plot."""
     u, t = solver(I=I, a=a, T=T, dt=dt, theta=theta)
 
-    t_e = linspace(0, T, 1001)        # fine mesh for u_e
+    t_e = np.linspace(0, T, 1001)        # fine mesh for u_e
     u_e = u_exact(t_e, I, a)
 
-    plot(t,   u,   'r--o',            # red dashes w/circles
+    plt.plot(t,   u,   'r--o',            # red dashes w/circles
          t_e, u_e, 'b-')              # blue line for exact sol.
-    legend(['numerical', 'exact'])
-    xlabel('t')
-    ylabel('u')
-    title('theta=%g, dt=%g' % (theta, dt))
-    savefig('plot_%s_%g.png' % (theta, dt))
+    plt.legend(['numerical', 'exact'])
+    plt.xlabel('t')
+    plt.ylabel('u')
+    plt.title('theta=%g, dt=%g' % (theta, dt))
+    plt.savefig('plot_%s_%g.png' % (theta, dt))
 
 def test_solver_three_steps():
     """Compare three steps with known manual computations."""
     theta = 0.8; a = 2; I = 0.1; dt = 0.8
-    u_by_hand = array([I,
+    u_by_hand = np.array([I,
                        0.0298245614035,
                        0.00889504462912,
                        0.00265290804728])
