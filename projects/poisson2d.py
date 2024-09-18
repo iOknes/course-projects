682c511c6ad57db02b51be447f9b6d8b95522838
Iver Oknes <iver@oknes.no>
2024-09-18 11:54:04 +0200

    Add poisson 2d solver with basic test

--- projects/poisson2d.py
+++ projects/poisson2d.py
@@ -7,6 +7,12 @@ from poisson import Poisson
 
 x, y = sp.symbols('x,y')
 
+def D2(N):
+    D = sparse.diags([1, -2, 1], [-1, 0, 1], (N+1, N+1), 'lil')
+    D[0, :4] = 2, -5, 4, -1
+    D[-1, -4:] = -1, 4, -5, 2
+    return D
+
 class Poisson2D:
     r"""Solve Poisson's equation in 2D::
 
@@ -18,19 +24,43 @@ class Poisson2D:
     def __init__(self, Lx, Ly, Nx, Ny):
         self.px = Poisson(Lx, Nx) # we can reuse some of the code from the 1D case
         self.py = Poisson(Ly, Ny)
+        self.Lx = Lx
+        self.Ly = Ly
+        self.Nx = Nx
+        self.Ny = Ny
+        self.dx = Lx / Nx
+        self.dy = Ly / Ny
 
     def create_mesh(self):
         """Return a 2D Cartesian mesh
         """
-        raise NotImplementedError
+        x = np.linspace(0, self.Lx, self.Nx+1)
+        y = np.linspace(0, self.Ly, self.Ny+1)
+        return np.meshgrid(x, y, indexing='ij')
 
     def laplace(self):
         """Return a vectorized Laplace operator"""
-        raise NotImplementedError
+        D2x = (1./self.dx**2)*D2(self.Nx)
+        D2y = (1./self.dy**2)*D2(self.Ny)
+        return (sparse.kron(D2x, sparse.eye(self.Ny+1)) +
+            sparse.kron(sparse.eye(self.Nx+1), D2y))
 
     def assemble(self, f=None):
         """Return assemble coefficient matrix A and right hand side vector b"""
-        raise NotImplementedError
+        A = self.laplace()
+        B = np.ones((self.Nx+1, self.Ny+1), dtype=bool)
+        B[1:-1, 1:-1] = 0
+        bnds = np.where(B.ravel() == 1)[0]
+        xij, yij = self.create_mesh()
+        F = sp.lambdify((x, y), f)(xij, yij)
+        A = A.tolil()
+        for i in bnds:
+            A[i] = 0
+            A[i, i] = 1
+        A = A.tocsr()
+        b = F.ravel()
+        b[bnds] = 0
+        return A, b
 
     def l2_error(self, u, ue):
         """Return l2-error
@@ -63,5 +93,23 @@ class Poisson2D:
         return sparse.linalg.spsolve(A, b.ravel()).reshape((self.px.N+1, self.py.N+1))
 
 def test_poisson2d():
-    assert False
+    ue = x*(1-x)*y*(1-y)*sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
+    f = ue.diff(x, 2) + ue.diff(y, 2)
+    sol = Poisson2D(1, 1, 30, 30)
+    u = sol(f)
+    xij, yij = sol.create_mesh()
+    assert np.allclose(u, sp.lambdify((x, y), ue)(xij, yij), atol=0.01)
 
+def main():
+    ue = x*(1-x)*y*(1-y)*sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
+    f = ue.diff(x, 2) + ue.diff(y, 2)
+    sol = Poisson2D(1, 1, 30, 30)
+    u = sol(f)
+    xij, yij = sol.create_mesh()
+    #plt.contourf(xij, yij, u)
+    plt.contourf(xij, yij, u - sp.lambdify((x,y), ue)(xij, yij))
+    plt.colorbar()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
